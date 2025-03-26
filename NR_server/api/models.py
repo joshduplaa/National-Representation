2b7c8cd0fa666566053b235f3c098e27a304be29
Joshua Duplaa <joshuaduplaa@outlook.com>
2025-03-26 12:50:51 -0500

    made migrations for API object models

--- NR_server/api/models.py
+++ NR_server/api/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.username
+        return self.name
 
 class Poll():
 
--- NR_server/api/views.py
+++ NR_server/api/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
 
 def index(request):
     return HttpResponse("Hello, world. You're at the starting API index.")
+
+@api_view(['GET'])
+def get_user(request):
+    return Response(UserSerializer({"user": "jduplaa", "password": "abc123"}))
+
+
+@api_view(['GET'])
+def get_poll(request):
+    return Response(UserSerializer({"title": "human rights"}))
+
